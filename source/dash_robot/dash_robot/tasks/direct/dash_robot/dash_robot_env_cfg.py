cde674e56edc8cb546369ee514fced7bf7821aaf
Jonathan Li <jonathanli668899@gmail.com>
2025-11-26 20:44:49 -0500

    initial commit

---
+++ source/dash_robot/dash_robot/tasks/direct/dash_robot/dash_robot_env_cfg.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from isaaclab_assets.robots.dash import DASH_CFG
+
+from isaaclab.assets import ArticulationCfg
+from isaaclab.envs import DirectRLEnvCfg
+from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.sim import SimulationCfg
+from isaaclab.utils import configclass
+
+
+@configclass
+class DashRobotEnvCfg(DirectRLEnvCfg):
+    # env
+    decimation = 2
+    episode_length_s = 5.0
+
+    # - spaces definition
+    action_space = 1
+    observation_space = 4
+    state_space = 0
+
+    # simulation
+    sim: SimulationCfg = SimulationCfg(dt=1 / 120, render_interval=decimation)
+
+    # robot(s)
+    robot_cfg: ArticulationCfg = DASH_CFG.replace(prim_path="/World/envs/env_.*/Robot")
+
+    # scene
+    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=4096, env_spacing=4.0, replicate_physics=True)
+
+    # custom parameters/scales
+    # - controllable joint
+    left_joint_names = [
+        "l_hip_yaw", 
+        "l_hip_roll", 
+        "l_hip_pitch", 
+        "l_knee_pitch", 
+        "l_ankle_pitch", 
+        "l_shoulder_pitch",
+        "l_shoulder_roll",
+        "l_shoulder_yaw",
+        "l_elbow_pitch"
+    ]
+    right_joint_names = [
+        "r_hip_yaw",
+        "r_hip_roll",
+        "r_hip_pitch",
+        "r_knee_pitch",
+        "r_ankle_pitch",
+        "r_shoulder_pitch",
+        "r_shoulder_roll",
+        "r_shoulder_yaw",
+        "r_elbow_pitch"
+    ]
+    # - action scale
+    action_scale = 100.0  # [N]
+
+    # - Reward scales, add velocity and joint movement rewards?
+    rew_scale_alive = 1.0
+    rew_scale_terminated = -2.0
